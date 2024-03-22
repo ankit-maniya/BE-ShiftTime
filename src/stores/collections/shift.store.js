e29a416d2bc91f67374c29178d001f897df57876
Ankit Maniya <66209504+ankit-maniya@users.noreply.github.com>
2024-03-22 00:59:14 -0400

    Store Module Created

---
+++ src/stores/collections/shift.store.js
@@ -0,0 +1,76 @@
+"use strict";
+
+/*** Third Party Packages ***/
+import moment from "moment";
+
+/*** Global ***/
+import utils from "../../global/index.js";
+
+/*** Model Schema ***/
+import { Shift } from "../../models/index.js";
+
+/*** BaseModal ***/
+import BaseModal from "../base.store.js";
+
+class ShiftStore extends BaseModal {
+  /*** NOTE: it's require to declare whenever you extends BaseModal ***/
+  model = Shift;
+
+  getAll = async (query, projection, sort) => {
+    try {
+      return await super.getAll(query, projection, null, sort);
+    } catch (exception) {
+      utils.throwError(500, "", "Error while getting Shift")(exception);
+    }
+  };
+
+  getWithSort = async (query, sortBy) => {
+    try {
+      return await this.model.find(query).sort(sortBy);
+    } catch (exception) {
+      utils.throwError(500, "", "Error while getting single Shift")(exception);
+    }
+  };
+
+  get = async (query) => {
+    try {
+      return await super.get(query);
+    } catch (exception) {
+      utils.throwError(500, "", "Error while getting single Shift")(exception);
+    }
+  };
+
+  create = async (objectToCreate) => {
+    try {
+      if (!objectToCreate.createdOn) {
+        objectToCreate.createdOn = moment().valueOf();
+      }
+
+      return await super.create(objectToCreate);
+    } catch (exception) {
+      utils.throwError(500, "", "Error while creating Shift")(exception);
+    }
+  };
+
+  update = async (id, whatToUpdate, whatDoDelete) => {
+    try {
+      if (!whatToUpdate.updatedOn) {
+        whatToUpdate.updatedOn = moment().valueOf();
+      }
+
+      return await super.update(id, whatToUpdate, whatDoDelete);
+    } catch (exception) {
+      utils.throwError(500, "", "Error while updating Shift")(exception);
+    }
+  };
+
+  delete = async (ShiftId) => {
+    try {
+      return await super.delete(ShiftId);
+    } catch (exception) {
+      utils.throwError(500, "", "Error while deleting Shift")(exception);
+    }
+  };
+}
+
+export default new ShiftStore();
--- src/stores/index.js
+++ src/stores/index.js
@@ -6,6 +6,7 @@ import UserStore from "./collections/user.store.js"
 import AvailabilityStore from "./collections/availability.store.js"
 import CategoryStore from "./collections/category.store.js"
 import BookOffStore from "./collections/bookoff.store.js"
+import ShiftStore from "./collections/shift.store.js"
 
 /*** Config ***/
 import { config } from "../configs/index.js"
@@ -20,5 +21,6 @@ export {
     UserStore,
     AvailabilityStore,
     CategoryStore,
-    BookOffStore
+    BookOffStore,
+    ShiftStore
 }
