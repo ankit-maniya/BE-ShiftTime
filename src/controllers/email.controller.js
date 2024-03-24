84579ae3ca4aa622307472f9671790c89db886b1
Ankit Maniya <66209504+ankit-maniya@users.noreply.github.com>
2024-03-24 00:58:23 -0400

    Email controller created

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -1,5 +1,7 @@
 /*** Global ***/
 import EmailService from '../global/EmailService.js';
+import EmailTempletes from '../global/EmailTempletes.js';
+import { config } from '../configs/index.js';
 import utils from '../global/index.js'
 
 class EmailController {
@@ -11,15 +13,71 @@ class EmailController {
             emailType = req.body.emailType;
             subject = req.body.subject;
             text = req.body.text;
+            html = req.body.html;
 
             await EmailService.sendMail({
                 from,
                 to,
                 subject,
-                text
+                text,
+                html
             })
 
-            utils.sendSuccess(res, 200, { label: 'Email sent successfully!' })
+            utils.sendSuccess(res, 200, { message: 'Email sent successfully!' })
+        } catch (exception) {
+            utils.sendError(res, 500)(exception)
+        }
+    }
+
+    sendShiftCreatedEmail = async (req, res) => {
+        try {
+            const { reciverEmail, subject } = req.body
+            const html = await EmailTempletes.createShiftIsCreatedEmail({});
+
+            await EmailService.sendMail({
+                from: config.EMAIL_USER,
+                to: reciverEmail,
+                subject: subject || "Shift has been created",
+                html
+            })
+
+            utils.sendSuccess(res, 200, { message: 'Email sent successfully!' })
+        } catch (exception) {
+            utils.sendError(res, 500)(exception)
+        }
+    }
+
+    sendAvailabilityStatusChangedEmail = async (req, res) => {
+        try {
+            const { reciverEmail, subject } = req.body
+            const html = EmailTempletes.createAvailabilityStatusChangeEmail({});
+
+            await EmailService.sendMail({
+                from: config.EMAIL_USER,
+                to: reciverEmail,
+                subject: subject,
+                html
+            })
+
+            utils.sendSuccess(res, 200, { message: 'Email sent successfully!' })
+        } catch (exception) {
+            utils.sendError(res, 500)(exception)
+        }
+    }
+
+    sendBookOffStatusChangedEmail = async (req, res) => {
+        try {
+            const { reciverEmail, subject } = req.body
+            const html = EmailTempletes.createBookoffStatusChangeEmail({});
+
+            await EmailService.sendMail({
+                from: config.EMAIL_USER,
+                to: reciverEmail,
+                subject: subject,
+                html
+            })
+
+            utils.sendSuccess(res, 200, { message: 'Email sent successfully!' })
         } catch (exception) {
             utils.sendError(res, 500)(exception)
         }
